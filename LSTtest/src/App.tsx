c0798858163dbb4af3685def82a5f7c599d9230c
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-10-10 12:45:50 +0300

    add path to route

--- LSTtest/src/App.tsx
+++ LSTtest/src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Search />} />
+        <Route path="/LSTtest" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
